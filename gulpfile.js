9245f27348cce5e2c0df53b3fa02b92190d4952c
Emmanuel <emmanuel.lazarte.012@gmail.com>
2021-03-31 00:50:00 -0300

    Borramos node_modules y arreglamos estructuras

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require("gulp");
 const concat = require('gulp-concat'); // agrupa varios archivos en uno solo
-const terser = require('gulp-terser'); // minifica js, alternativa quizas uglify
+// const terser = require('gulp-terser'); // minifica js, alternativa quizas uglify
 const sourcemaps = require('gulp-sourcemaps');
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
@@ -13,24 +13,32 @@ const {src, series, parallel, dest, watch } = require('gulp');
 const jsPath = 'src/assets/js/**/*.js';
 const cssPath = 'src/assets/css/**/*.css';
 const scssPath = 'src/assets/scss/**/*.scss';
+const viewsPath = 'src/views/*.html';
+const componentsPath = 'src/components/*.html';
 
-function copyHtml(){
-  return src('src/*.html').pipe(gulp.dest('dist'));
+function copyViews(){
+  return src(viewsPath).pipe(gulp.dest('dist'));
+}
+
+function copyComponents(){
+    return src(componentsPath).pipe(gulp.dest('dist'));
 }
 
 function jsTask() {
-    return src(jsPath) // ruta origen de los archivos a copiar
+    return src(['node_modules/jquery/dist/jquery.min.js',
+                'node_modules/bootstrap/dist/js/bootstrap.min.js',
+                jsPath]) // ruta origen de los archivos a copiar
     // pipe actúa como un "then"
         .pipe(sourcemaps.init()) //
         .pipe(concat('all.js')) // archivo donde se agruparan todos los js
-        .pipe(terser()) // minificamos
+        // .pipe(terser()) // minificamos
         .pipe(sourcemaps.write('.')) // crea el archivo en la ruta actual
         .pipe(dest('dist/assets/js')); // copiamos el archivo a la ruta destino
 }
 
 
 function cssTask() {
-    return src(cssPath)
+    return src([cssPath, 'node_modules/bootstrap/dist/css/bootstrap.css'])
         .pipe(sourcemaps.init())
         .pipe(concat('style.css'))
         .pipe(postcss([cssnano()])) // minificamos la hoja de estilos
@@ -60,19 +68,21 @@ function browsersyncReload(cb){
 
 // al observar cambios en los archivos, ejecutará esas tareas
 function watchTask() {
-    watch('*.html', browsersyncReload);
-    watch([cssPath, scssPath, jsPath],{ interval: 1000 }, parallel(cssTask, scssTask, jsTask, browsersyncReload));
+    // watch('*.html', browsersyncReload);
+    watch([viewsPath, componentsPath, cssPath, scssPath, jsPath],
+          parallel(copyViews, copyComponents, cssTask, scssTask, jsTask, browsersyncReload));
 }
 
 // cualquiera de las tareas que exportamos,
 // se pueden ejecutar por separado en la terminal con: gulp nombreTarea
-exports.copyHtml = copyHtml;
+exports.copyViews = copyViews;
+exports.copyComponents = copyComponents;
 exports.jsTask = jsTask;
 exports.cssTask = cssTask;
 exports.scssTask = scssTask;
 // usamos parallel para multiples tareas
 exports.default = series(
-    parallel(copyHtml, jsTask, cssTask, scssTask),
+    parallel(copyViews, copyComponents, jsTask, cssTask, scssTask),
     // parallel(copyHtml, jsTask, mergeCssTask),
     // se quedará la terminal ejecutandose y observando cambios en los archivos
     browsersyncServe,
